99d72e107ce65d5d7cb7441ee2a2ee1eb9013d25
sjaaktrekhaak1 <peters.jeffrey@live.nl>
2017-06-13 13:30:42 +0200

    Update emailtocsv.py

--- emailtocsv.py
+++ emailtocsv.py
@@ -1,7 +1,13 @@
 import mailbox
 import csv
+import os
+import sys
+from email.header import decode_header
+from pandas import read_csv
+import sqlite3
 
-def more_payloads(message):
+
+'''def more_payloads(message):
 	body = ""
 	if message.is_multipart():
 		for payload in message.get_payload():
@@ -10,22 +16,41 @@ def more_payloads(message):
 		if message.get_content_type() == 'text/plain':
 			body += message.get_payload(decode=True)
         return body
-
+'''
+def getbody(message): #getting plain text 'email body'
+    body = None
+    if message.is_multipart():
+        for part in message.walk():
+            if part.is_multipart():
+                for subpart in part.walk():
+                    if subpart.get_content_type() == 'text/plain':
+                        body = subpart.get_payload(decode=True)
+            elif part.get_content_type() == 'text/plain':
+                body = part.get_payload(decode=True)
+    elif message.get_content_type() == 'text/plain':
+        body = message.get_payload(decode=True)
+    return body
 
 writer =csv.writer(open("clean_mail.csv", "wb"))
-for message in mailbox.mbox('alice.mbox'):
-    body = more_payloads(message)
-    writer.writerow([message['subject'], message['from'], message['to'], message['date'], body])
 
-'''writer =csv.writer(open("clean_mailpst4.csv", "wb"))
-for message in mailbox.mbox('Outbox'):
-    writer.writerow([message['subject'], message['from'], message['to'], message['date']])
+dirt = './MAIL/'
+for root,dirs, filenames in os.walk(dirt):
+    for f in filenames:
+        if f.endswith('.mbox'):
+            mboxfile = f
+            print (f)
 
-writer =csv.writer(open("clean_mailpst5.csv", "wb"))
-for message in mailbox.mbox('Sent Items'):
-    writer.writerow([message['subject'], message['from'], message['to'], message['date']])
+            for message in mailbox.mbox(mboxfile):
+                body = getbody(message)
+                writer.writerow([message['subject'], message['from'], message['to'], message['date'], body])
 
-writer =csv.writer(open("clean_mailpst6.csv", "wb"))
-for message in mailbox.mbox('Personal Folders'):
-    writer.writerow([message['subject'], message['from'], message['to'], message['date']])
-'''
+
+
+
+
+
+
+
+
+
+
