93221fbb1343738e43cd0d00fc2bab79d5fa88b3
LinseyvO <liinseyy17@gmail.com>
2017-06-15 13:26:46 +0200

    Create main.py

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import tkFileDialog
 import tkSimpleDialog
 import tkMessageBox
 import os
-#import laden_image
+import subprocess as sub
+
+#Maak de GUI
+root = Tk()
+w = Label(root, fg='blue', bg='white', text="Ipfit5")
+w.pack()
 
 def quit():
     root.destroy()
@@ -12,7 +17,6 @@ def openBestand():
     root.filename = tkFileDialog.askopenfilename(filetypes=(("E01 File", "*.E0*"), ("AFF", "*.aff"), ("RAW", ".raw"), ("All files", "*.*")))
     f = open(root.filename)
     print(root.filename) #geeft pad naar bestand
-
 
     # Het imagebestand opgeven
     output = "python laden_image.py -i "
@@ -29,10 +33,33 @@ def openBestand():
     print output
     os.system(output)
 
-#Maak de GUI
-root = Tk()
-w = Label(root, text="Ipfit5")
-w.pack()
+def emlnaarmbox():
+    os.system('python emltombox.py')
+
+def emailtocsv():
+    os.system('python emailtocsv.py')
+
+def writeheader():
+    os.system('python writeheader.py')
+
+def insertintodatabase():
+    os.system('python insertintodatabase.py')
+
+#Button voor emlnaarmbox
+B = Button(root, pady=10, fg='blue', bg='white', text="Stap 1: Eml naar mbox", command=emlnaarmbox)
+B.pack()
+
+#Button voor mboxnaarcsv
+B2 = Button(root, pady=10, fg='blue', bg='white', text="Stap 2: Mbox naar csv", command=emailtocsv)
+B2.pack()
+
+#Button voor schrijvenheader
+B3 = Button(root, pady=10, fg='blue', bg='white', text="Stap 3: Schrijven van header", command=writeheader)
+B3.pack()
+
+#Button voor database
+B4 = Button(root, pady=10,  fg='blue', bg='white', text="Stap 4: Database", command=insertintodatabase)
+B4.pack()
 
 #Menu code
 menu = Menu(root)
@@ -40,16 +67,11 @@ root.config(menu=menu)
 
 subMenu = Menu(menu)
 menu.add_cascade(label="Bestand", menu=subMenu)
-#subMenu.add_command(label="Inladen", command=laden_image)
 subMenu.add_command(label="Open bestand", command=openBestand)
 
 subMenu.add_separator()
 
 subMenu.add_command(label="Exit", command=quit)
-
-#Status bar
-#status = Label(root, text="Bezig...", bd=1, relief=SUNKEN, anchor=W)
-#status.pack(side=BOTTOM, fill=X)
 
 root.geometry('{}x{}'.format(500, 300))
 root.mainloop()
