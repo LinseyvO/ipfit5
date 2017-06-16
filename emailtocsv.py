b944e3844c736a308faa625c42c1b6d84d6ee7be
sjaaktrekhaak1 <peters.jeffrey@live.nl>
2017-06-16 13:31:22 +0200

    Update emailtocsv.py

--- emailtocsv.py
+++ emailtocsv.py
@@ -6,18 +6,7 @@ from email.header import decode_header
 from pandas import read_csv
 import sqlite3
 
-
-'''def more_payloads(message):
-	body = ""
-	if message.is_multipart():
-		for payload in message.get_payload():
-			body += more_payloads(payload)
-	else:
-		if message.get_content_type() == 'text/plain':
-			body += message.get_payload(decode=True)
-        return body
-'''
-def getbody(message): #getting plain text 'email body'
+def getbody(message): # haal de body uit het e-mail bestand
     body = None
     if message.is_multipart():
         for part in message.walk():
@@ -31,16 +20,16 @@ def getbody(message): #getting plain text 'email body'
         body = message.get_payload(decode=True)
     return body
 
-writer =csv.writer(open("clean_mail.csv", "wb"))
+writer =csv.writer(open("clean_mail.csv", "wb")) # er wordt een csv bestand aangemaakt en er kan nu data naar worden geschreven
 
-dirt = './MAIL/'
-for root,dirs, filenames in os.walk(dirt):
+dirt = './MAIL/' # er wordt een variabel aangemaakt waarin het pad wordt gezet
+for root,dirs, filenames in os.walk(dirt): # deze loop loopt door opgegeven map heen en zoekt naar bestanden die eindigen met de extensie '.mbox'
     for f in filenames:
         if f.endswith('.mbox'):
-            mboxfile = f
-            print (f)
+            mboxfile = f # deze variabele wordt gelijk gemaakt aan de naam van het gevonden bestand dat eindigt op '.mbox'
+            print (f) # voor controle wordt de naam van het gevonden mbox bestand naar de terminal geprint
 
-            for message in mailbox.mbox(mboxfile):
+            for message in mailbox.mbox(mboxfile): # hier wordt de data naar het csv bestand weggeschreven
                 body = getbody(message)
                 writer.writerow([message['subject'], message['from'], message['to'], message['date'], body])
 
