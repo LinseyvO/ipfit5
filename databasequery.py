3618a33a69597597431f9ac1061927345486a526
sjaaktrekhaak1 <peters.jeffrey@live.nl>
2017-06-30 13:44:28 +0200

    Add files via upload

--- databasequery.py
+++ databasequery.py
@@ -1,17 +1,16 @@
 import sqlite3
 
-#er wordt een verbinding opgezet met de database
 conn = sqlite3.connect('email.db')
 curs = conn.cursor()
 
-#er wordt een query uitgevoerd
-curs.execute('SELECT * from Mail')
+#curs.execute('SELECT * from Mail order by date ASC')
+curs.execute('Select * from Users')
+iets = curs.fetchall()
 
-#hier wordt de opgevraagde query uitgeprint in de terminal
-for row in curs:
+for row in iets:
     print (row)
 
 
 
 
-conn.close() # de connectie wordt afgesloten
+conn.close()
